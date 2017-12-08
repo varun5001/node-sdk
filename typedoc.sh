dbf979d7df6127e1bde5af374d36ea32340e5b87
Anwesha Naskar <Anwesha.Naskar@ibm.com>
2017-12-08 10:19:23 -0500

    Adds custom theme for typedoc formatting

--- typedoc.sh
+++ typedoc.sh
@@ -1,6 +1,8 @@
-./node_modules/.bin/typedoc --mode file --theme minimal --exclude "./lib/*.ts" --out ./doc ./authorization/v1.ts \
+./node_modules/.bin/typedoc --mode file --theme ./custom-typedoc-theme --excludeExternals \
+    --out ./doc ./authorization/v1.ts \
     ./conversation/v1-generated.ts ./discovery/v1-generated.ts \
     ./language-translator/v2-generated.ts ./natural-language-classifier/v1-generated.ts \
     ./natural-language-understanding/v1.ts ./personality-insights/v3-generated.ts \
     ./speech-to-text/v1-generated.ts ./text-to-speech/v1-generated.ts \
-    ./tone-analyzer/v3-generated.ts ./visual-recognition/v3-generated.ts
+    ./tone-analyzer/v3-generated.ts ./visual-recognition/v3-generated.ts \
+    --target "ES5"
